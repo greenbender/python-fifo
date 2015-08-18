cac58b50df55651afb38673e75aa176eaacc4b65
greenbender <byron.platt@gmail.com>
2015-08-18 15:43:37 +1000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='fifo',
+    version='0.8.0',
+    description='A simple and reasonably efficient FIFO',
+    author='Byron Platt',
+    license='GPL2',
+    url='https://github.com/greenbender/python-fifo',
+    packages=['fifo'],
+)
